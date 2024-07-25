dda10bd12c9b7d3a1d0bfa8b79f6bf3ccbeaf053
Patrick <psylantys@gmail.com>
2024-07-25 11:28:01 -0400

    test

--- public/content_script.js
+++ public/content_script.js
@@ -1,4 +1,4 @@
-const apiKey = "";
+const apiKey = "sk-proj-xGeorjPeEbFH5GnFtKN2T3BlbkFJbIoAmXwghFY5eE5oCcS3";
 const googleUrl = new URL(window.location.href);
 const search = googleUrl.search;
 const searchParam = new URLSearchParams(search);
